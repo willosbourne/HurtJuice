0f10da84f4c4af69fe2d303cbd59cbc307393c2a
Will Osbourne <will@osbourne.org>
2024-03-06 15:28:46 -0500

    init w/ functions

---
+++ GetToken.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace HurtJuice;
+
+public class GetToken
+{
+    private readonly ILogger _logger;
+
+    public GetToken(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<GetToken>();
+    }
+
+    [Function("GetToken")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        FunctionContext executionContext)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        response.WriteString("Welcome to Azure Functions!");
+
+        return response;
+        
+    }
+}
---
+++ ValidateToken.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace HurtJuice;
+
+public class ValidateToken
+{
+    private readonly ILogger _logger;
+
+    public ValidateToken(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<ValidateToken>();
+    }
+
+    [Function("ValidateToken")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        FunctionContext executionContext)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        response.WriteString("Welcome to Azure Functions!");
+
+        return response;
+        
+    }
+}
