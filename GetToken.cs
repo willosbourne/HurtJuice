d5dbf9139e1d31dc5ba220b5859e17ecccfe25e3
Will Osbourne <will@osbourne.org>
2024-03-07 17:52:05 -0500

    initial hurtjuice redirect url return

--- GetToken.cs
+++ GetToken.cs
@@ -1,18 +1,26 @@
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace HurtJuice;
 
 public class GetToken
 {
     private readonly ILogger _logger;
+    private readonly string _jwtUrl;
+    private readonly string _jwtSecret;
 
     public GetToken(ILoggerFactory loggerFactory)
     {
         _logger = loggerFactory.CreateLogger<GetToken>();
+        _jwtUrl = "https://helpjuice.com/jwt/sht-connect";
+        _jwtSecret = Environment.GetEnvironmentVariable("HELPJUICE_API_SECRET") ?? string.Empty;
     }
 
     [Function("GetToken")]
@@ -24,9 +32,50 @@ public class GetToken
         var response = req.CreateResponse(HttpStatusCode.OK);
         response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-        response.WriteString(Guid.NewGuid().ToString());
+        var redirectUrl = "didn't get url";
+
+        if (true)
+        {
+            redirectUrl = GetHelpjuiceRedirectUrl("will+sc-test1@sht.dev");
+        }
+
+        response.WriteString(redirectUrl);
 
         return response;
-        
+    }
+    
+    private string GetHelpjuiceRedirectUrl(string email, string fallbackUrl = "")
+    {
+        if (string.IsNullOrEmpty(fallbackUrl))
+        {
+            fallbackUrl = $"https://sht-connect.helpjuice.com";
+        }
+            
+        String jwt = GenerateHelpjuiceJwtToken(email);
+        String url = $"{_jwtUrl}?jwt={jwt}";
+            
+        if (!string.IsNullOrEmpty(fallbackUrl))
+        {
+            url = $"{url}&fallback_url={fallbackUrl}";
+        }
+            
+        return url;
+    }
+    
+    public String GenerateHelpjuiceJwtToken(String email)
+    {
+        JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+        Byte[] key = Encoding.ASCII.GetBytes(_jwtSecret);
+        SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[] {
+                new Claim("email", email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
     }
 }
