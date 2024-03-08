305c3b09bfb72de82a1f144d97bd69fe471cb808
Will Osbourne <will@osbourne.org>
2024-03-08 08:28:21 -0500

    embadden the route

--- GetToken.cs
+++ GetToken.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Net;
 using System.Security.Claims;
 using System.Text;
+using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -15,6 +16,7 @@ public class GetToken
     private readonly ILogger _logger;
     private readonly string _jwtUrl;
     private readonly string _jwtSecret;
+    private static readonly Random Random = new Random();
 
     public GetToken(ILoggerFactory loggerFactory)
     {
@@ -22,22 +24,43 @@ public class GetToken
         _jwtUrl = "https://helpjuice.com/jwt/sht-connect";
         _jwtSecret = Environment.GetEnvironmentVariable("HELPJUICE_API_SECRET") ?? string.Empty;
     }
+    
+    public class RequestBody
+    {
+        public string email { get; set; }
+    }
 
     [Function("GetToken")]
-    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
         FunctionContext executionContext)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
+        if (Random.NextDouble() < 0.3)
+        {
+            var badResponse = req.CreateResponse((HttpStatusCode) 418);
+            badResponse.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            return badResponse;
+        }
+        
+        string requestBodyString = await new StreamReader(req.Body).ReadToEndAsync();
+        RequestBody? requestBody = JsonSerializer.Deserialize<RequestBody>(requestBodyString);
 
         var response = req.CreateResponse(HttpStatusCode.OK);
         response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
         var redirectUrl = "didn't get url";
-
-        if (true)
+        if (Random.NextDouble() < 0.2)
         {
-            redirectUrl = GetHelpjuiceRedirectUrl("will+sc-test1@sht.dev");
+            redirectUrl = "service unavailable";
+        }
+        else if (!string.IsNullOrEmpty(requestBody.email))
+        {
+            redirectUrl = GetHelpjuiceRedirectUrl(requestBody.email);
             redirectUrl = StringReplacer.ReplaceMiddleRandomly(redirectUrl, _logger);
+        }
+        else
+        {
+            redirectUrl = "bad email";
         }
 
         response.WriteString(redirectUrl);
--- ValidateToken.cs
+++ ValidateToken.cs
@@ -38,7 +38,9 @@ public class ValidateToken
 
         var url = requestBody?.url ?? string.Empty;
 
-        if (StringReplacer.HasReplacementInString(url) || string.IsNullOrEmpty(url))
+        //helpjuice.com/jwt/sht-connect?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IndpbGxvc2JvdXJuZUBzZXZlbmhpbGxzdGVjaG5vbG9neS5jb20iLCJqdGkiOiJhODdmMDVhZi1mZTZjLTQwZjYtYWJjMy1mYzVlNTczZThlNDAiLCJuYmYiOjE3MDk5MDQzNTksImV4cCI6MTcxMDUwOTE1OSwiaWF0IjoxNzA5OTA0MzU5fQ.yY5g119CxEwvECSjgF-EBJ9Y1W_n-H6HVLsJ36nmLUM&fallback_url=https://sht-connect.helpjuice.com
+
+        if (StringReplacer.HasReplacementInString(url) || string.IsNullOrEmpty(url) || !url.Contains("helpjuice.com"))
         {
             _logger.LogInformation("----------------Invalid URL----------------");
             await response.WriteStringAsync("false");
